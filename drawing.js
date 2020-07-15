4bf89ebd23c0eaf84bf95554bb173e6380973e6f
clocks-in-a-cooler <34520271+Clocks-in-a-cooler@users.noreply.github.com>
2020-07-15 11:18:38 -0400

    drawing and messaging the player is working.

---
+++ drawing.js
@@ -0,0 +1,102 @@
+function create_drawing_canvas(width, height) {
+    var canvas = document.createElement("canvas");
+    
+    canvas.width = width * 25;
+    canvas.height = height * 25;
+    
+    canvas.className += "drawing-canvas";
+    
+    return canvas;
+}
+
+function draw_sprite(width, height, update) {
+    //shows the player the canvas for drawing, plus the tools for drawing.
+    //when the player is done, call the update function with the base 64 data of the drawn image.
+    //update is a callback, given the async nature of this whole thing.
+    
+    var canvas           = create_drawing_canvas(width, height);
+    var done_button      = document.createElement("button"); done_button.innerHTML = "done";
+    var drawing_wrapper  = document.getElementById("drawing-wrapper");
+    var drawing_controls = document.getElementById("drawing-controls");
+    
+    drawing_wrapper.style.width  = (width * 25) + 100 + 12 + "px";
+    drawing_wrapper.style.height = (height * 25) + 50 + 2 + "px";
+    
+    drawing_wrapper.appendChild(canvas); drawing_controls.appendChild(done_button);
+    drawing_wrapper.style.visibility = "visible";
+    
+    context = canvas.getContext("2d");
+    
+    context.lineWidth   = 3;
+    context.strokeStyle = "black";
+    
+    canvas.addEventListener("mousedown", mousedown);
+    canvas.addEventListener("mouseup", mouseup);
+    canvas.addEventListener("mousemove", mousemove);
+    
+    done_button.addEventListener("click", () => {
+        update(canvas.toDataURL());
+        drawing_wrapper.style.visibility = "hidden";
+        
+        canvas.removeEventListener("mousedown", mousedown);
+        canvas.removeEventListener("mouseup", mouseup);
+        canvas.removeEventListener("mousemove", mousemove);
+        
+        drawing_wrapper.removeChild(canvas); drawing_controls.removeChild(done_button);
+    });
+}
+
+// drawing on the canvas directly
+var mouse_is_down = false;
+var mouse_pos     = { x: null, y: null };
+
+var context = null;
+var tool = "pencil";
+var size = 3;
+
+function mousedown(evt) {
+    mouse_is_down = true;
+    mouse_pos.x = evt.offsetX; mouse_pos.y = evt.offsetY;
+}
+
+function mousemove(evt) {
+    if (!mouse_is_down) {
+        return;
+    }
+    
+    if (tool == "pencil") {
+        context.arc(mouse_pos.x, mouse_pos.y, size / 2, 0, Math.PI * 2);
+        context.beginPath();
+        context.moveTo(mouse_pos.x, mouse_pos.y);
+        context.lineTo(evt.offsetX, evt.offsetY);
+        context.closePath();
+        context.stroke();
+    }
+    
+    if (tool == "eraser") {
+        context.clearRect(mouse_pos.x - 10, mouse_pos.y - 10, 20, 20);
+    }
+    
+    mouse_pos.x = evt.offsetX; mouse_pos.y = evt.offsetY;
+    
+}
+
+function mouseup(evt) {
+    mouse_is_down = false;
+    mouse_pos.x = null; mouse_pos.y = null;
+}
+
+var pencil_button = document.getElementById("pencil");
+var eraser_button = document.getElementById("eraser");
+
+pencil_button.addEventListener("click", () => {
+    eraser_button.className = "";
+    pencil_button.className = "selected";
+    tool = "pencil";
+});
+
+eraser_button.addEventListener("click", () => {
+    pencil_button.className = "";
+    eraser_button.className = "selected";
+    tool = "eraser";
+});
---
+++ script.js
@@ -0,0 +1,28 @@
+function conversation(messages, after) {
+    //shows the player a series of text in #message
+    //after it's done, call the after function
+    
+    var elt = document.getElementById("message");
+    
+    var continue_button       = document.createElement("button");
+    continue_button.innerHTML = "...";
+    
+    var index = 0;
+    
+    function next_message() {
+        index++;
+        if (index < messages.length) {
+            message.innerHTML = messages[index];
+            message.appendChild(continue_button);
+        } else {
+            continue_button.removeEventListener("click", next_message);
+            message.innerHTML = "";
+            after();
+        }
+    }
+    
+    continue_button.addEventListener("click", next_message);
+    
+    message.innerHTML = messages[index];
+    message.appendChild(continue_button);
+}
